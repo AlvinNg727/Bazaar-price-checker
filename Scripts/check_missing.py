109cfe1dcd02ae67a6e2c20bdf4ee856e179fa5e
Alvin <58621029+AlvinNg727@users.noreply.github.com>
2020-06-19 23:59:58 +0800

    Added new items and made it auto loop

---
+++ Scripts/check_missing.py
@@ -0,0 +1,18 @@
+import requests
+from items import items
+
+api_key = "ba710f2d-6547-4936-8aab-72b01300a8a5"
+
+bazaarJson = requests.get("https://api.hypixel.net/skyblock/bazaar?key=" + api_key).json()
+
+final1 = []
+final2 = []
+
+for key in items:
+    value = items[key]
+    final1.append(value)
+
+for num2, i2 in enumerate(bazaarJson["products"]):
+    final2.append(i2)
+
+print(list(set(final2) - set(final1)))
--- Scripts/main.py
+++ Scripts/main.py
@@ -1,9 +1,10 @@
 import requests
 from items import items
+import asyncio
 
 api_key = "ba710f2d-6547-4936-8aab-72b01300a8a5"
 
-def check_average(json, productId):
+async def check_average(json, productId):
     #if there are no buy orders for the item, the buyPrice will = 0
     if len(json[productId]['sell_summary']) > 0:
         buyPrice = json[productId]["sell_summary"][0]["pricePerUnit"]
@@ -21,7 +22,7 @@ def check_average(json, productId):
 lowestAmount = 0
 
 #getting the lowest amount
-def getAmount():
+async def getAmount():
     global lowestAmount
     try:
         lowestAmount = int(input("What is the lowest amount of insta buys and insta sells: "))
@@ -40,47 +41,48 @@ page = 1
 names = []
 finalItems = []
 
-def Main():
-    global itemsList, names, finalItems, page
+async def Main():
+    await getAmount()
 
-    currentIndex = 0
-    itemsList.clear()
-    names.clear()
-    finalItems.clear()
-    item = ""
-    page = 1
+    while True:
+        global itemsList, names, finalItems, page
+        currentIndex = 0
+        itemsList.clear()
+        names.clear()
+        finalItems.clear()
+        item = ""
+        page = 1
 
-    getAmount()
+        bazaarJson = requests.get("https://api.hypixel.net/skyblock/bazaar?key=" + api_key).json()
+        bazaarJson = bazaarJson["products"]
 
-    bazaarJson = requests.get("https://api.hypixel.net/skyblock/bazaar?key=" + api_key).json()
-    bazaarJson = bazaarJson["products"]
+        #get
+        for key in items:
+            value = items[key]
+            if (bazaarJson[value]["quick_status"]["sellMovingWeek"] >= lowestAmount) and (bazaarJson[value]["quick_status"]["buyMovingWeek"] >= lowestAmount):
+                item = await check_average(bazaarJson, value)
+                itemsList.append([item, key])
+            currentIndex += 1
+            #print(str(round((currentIndex / 207 * 100), 1)) + "%")
 
-    #get
-    for key in items:
-        value = items[key]
-        if (bazaarJson[value]["quick_status"]["sellMovingWeek"] >= lowestAmount) and (bazaarJson[value]["quick_status"]["buyMovingWeek"] >= lowestAmount):
-            item = check_average(bazaarJson, value)
-            itemsList.append([item, key])
-        currentIndex += 1
-        print(str(round((currentIndex / 197 * 100), 1)) + "%")
+        #reverse the list
+        itemsList.sort(reverse=True)
 
-    #reverse the list
-    itemsList.sort(reverse=True)
+        for i in range(len(itemsList)):
+            names.append(itemsList[i][1])
+            finalItems.append(itemsList[i][0])
 
-    for i in range(len(itemsList)):
-        names.append(itemsList[i][1])
-        finalItems.append(itemsList[i][0])
+        await printLn(names, finalItems)
+        await asyncio.sleep(5)
+"""
+        again = input("Do you want to do this again? (y/n)")
+        if again == "y":
+            Main()
+        elif again == "n":
+            quit()
+"""
 
-    printLn(names, finalItems)
-
-    again = input("Do you want to do this again? (y/n)")
-    if again == "y":
-        Main()
-    elif again == "n":
-        quit()
-
-
-def printLn(name, price):
+async def printLn(name, price):
     global page
 
     maxPage = 0
@@ -89,7 +91,7 @@ def printLn(name, price):
         maxPage = (len(name) // 10) + 1
     elif len(name) % 10 == 0:
         maxPage = len(name) // 10
-
+    """
     if page <= maxPage:
         if page == maxPage:
             for i in range((page * 10) - 10, len(name)):
@@ -103,6 +105,17 @@ def printLn(name, price):
             else:
                 if more == "y":
                     page += 1
-                    printLn(name, price)
+                    await printLn(name, price)
+    """
+    for i in range(0, 10):
+        print(f"{str(i + 1)}. {str(name[i])}: {str(price[i])}\n")
 
-Main()
+loop = asyncio.get_event_loop()
+try:
+    asyncio.ensure_future(Main())
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+finally:
+    print("Closing Loop")
+    loop.close()
